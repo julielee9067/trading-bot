0741140786641126e3457230ca899b021b3c9511
Julie Lee <julielee9067@gmail.com>
2023-10-07 12:00:14 +0900

    Add sell/buy functionalities

--- trader.py
+++ trader.py
@@ -1,6 +1,7 @@
 import os
 
 from alpaca.data import StockHistoricalDataClient, StockLatestQuoteRequest
+from alpaca.trading import TradingClient, MarketOrderRequest, OrderSide, TimeInForce
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -11,13 +12,40 @@ secret_key = os.getenv("SECRET_KEY")
 
 class TradingBot:
     def __init__(self):
-        self.client = StockHistoricalDataClient(api_key, secret_key)
+        self.stock_history_client = StockHistoricalDataClient(api_key, secret_key)
+        self.trading_client = TradingClient(api_key, secret_key, paper=True)
 
-    def get_latest_quote(self, stock_symbol: str) -> float:
-        request = StockLatestQuoteRequest(symbol_or_symbols=stock_symbol)
-        a = self.client.get_stock_latest_quote(request)
+    def get_latest_quote(self, symbol: str) -> float:
+        request = StockLatestQuoteRequest(symbol_or_symbols=symbol)
+        a = self.stock_history_client.get_stock_latest_quote(request)
 
-        return a[stock_symbol].ask_price
+        return a[symbol].ask_price
+
+    def verify_account_status(self) -> None:
+        if self.trading_client.get_account().account_blocked:
+            raise ValueError("Account restricted for trading")
+
+    def sell_stock(self, symbol: str) -> None:
+        self.verify_account_status()
+        request = MarketOrderRequest(
+            symbol=symbol,
+            qty=1,
+            side=OrderSide.SELL,
+            time_in_force=TimeInForce.DAY
+        )
+        self.trading_client.submit_order(request)
+        print("Successfully submitted sell request")
+
+    def buy_stock(self, symbol: str) -> None:
+        self.verify_account_status()
+        request = MarketOrderRequest(
+            symbol=symbol,
+            qty=1,
+            side=OrderSide.BUY,
+            time_in_force=TimeInForce.DAY
+        )
+        self.trading_client.submit_order(request)
+        print("Successfully submitted buy request")
 
 
 if __name__ == "__main__":
